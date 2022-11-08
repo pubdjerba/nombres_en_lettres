e4077ab8f41e0736dd410052808bf973232e77fc
Faouzi <pubdjerba@gmail.com>
2022-11-08 16:55:23 +0100

    T9

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -1,7 +1,21 @@
 import React from "react"
-import { Container, Grid, Box } from "@mui/system"
+import { Container, Grid, Box } from "@mui/material"
 
 const PageBody = () => {
-  return <div>Hello</div>
+  return (
+    <Container>
+      <Grid my={3} container spacing={2}>
+        <Grid item sx={{ backgroundColor: "aqua" }} md={2}>
+          xs=8
+        </Grid>
+        <Grid item sx={{ backgroundColor: "pink" }} md={8}>
+          xs=4
+        </Grid>
+        <Grid item sx={{ backgroundColor: "silver" }} xs={2}>
+          xs=4
+        </Grid>
+      </Grid>
+    </Container>
+  )
 }
 export default PageBody
