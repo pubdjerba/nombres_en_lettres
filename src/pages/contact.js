acd42f8b59589ec103a74fa9fe637498db2cd350
azura <pubdjerba@gmail.com>
2022-11-11 12:33:53 +0100

    h454

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import Contact from "../components/Contact"
+import Contact from "../components/contact"
 
 const ContactPage = () => {
   return (
